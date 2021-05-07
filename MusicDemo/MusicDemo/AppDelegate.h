c16376dff349d013c06f96a1bd104e13f6cee727
CWig-yX13 <fengming98673@126.com>
2021-05-07 18:38:22 +0800

    update

---
+++ MusicDemo/MusicDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MusicDemo
+//
+//  Created by 卢浩 on 16/4/9.
+//  Copyright © 2016年 卢浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
